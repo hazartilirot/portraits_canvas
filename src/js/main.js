6645844e2b2cf3bb7dfbf6f7183d88cffd86cac7
hazartilirot <hazartilirot@gmail.com>
2021-08-02 11:55:25 +0200

    wire up the first modal popup to a group of buttons

---
+++ src/js/main.js
@@ -0,0 +1,7 @@
+import modals from './modals/modals';
+
+window.addEventListener('DOMContentLoaded', () => {
+  "use strict";
+  
+  modals();
+})
--- src/js/modals/modals.js
+++ src/js/modals/modals.js
@@ -2,36 +2,38 @@ import { getScrollWidth } from '../utilities';
 import { setModalProps } from '../utilities';
 import callModalByTime from './callModalByTime';
 
-export default (
-  buttonsSelector,
-  modalSelector,
-  closeSelector,
-  closeOverlayByClick
-) => {
-  const trigger = document.querySelectorAll(buttonsSelector);
-  const modal = document.querySelector(modalSelector);
-  const close = document.querySelector(closeSelector);
-  const windows = document.querySelectorAll('[data-modals]')
-  
+export default () => {
   const scrollWidth = getScrollWidth();
+
+  const setModal = (
+    triggerSelector,
+    modalSelector,
+    closeSelector,
+    closeOverlayByClick = true
+  ) => {
+    const trigger = document.querySelectorAll(triggerSelector);
+    const modal = document.querySelector(modalSelector);
+    const close = document.querySelector(closeSelector);
+    const windows = document.querySelectorAll('[data-modal]')
+    
+    trigger.forEach(i => {
+      i.addEventListener('click', e => {
+        e.target && e.preventDefault();
+
+        windows.forEach(e => e.style.display = 'none')
+
+        setModalProps(modal, 'block', 'hidden', `${scrollWidth}px`);
+      })
+    });
+
+    close.addEventListener('click', () =>
+      setModalProps(modal, 'none', 'auto', '0px'));
+
+    modal.addEventListener('click', e => {
+      if (e.target === modal && closeOverlayByClick)
+        windows.forEach(e => setModalProps(e, 'none', 'auto', '0px'))
+    });
+  };
   
-  trigger.forEach(i => {
-    i.addEventListener('click', e => {
-      e.target && e.preventDefault();
-      
-      windows.forEach(e => e.style.display = 'none')
-      
-      setModalProps(modal, 'block', 'hidden', `${scrollWidth}px`);
-    })
-  });
-  
-  close.addEventListener('click', () => 
-    setModalProps(modal, 'none', 'auto', '0px'));
-  
-  modal.addEventListener('click', e => {
-    if (e.target === modal && closeOverlayByClick)
-      windows.forEach(e => setModalProps(e, 'none', 'auto', '0px'))
-  });
-  
-  
-};
+  setModal('.button-design', '.popup-design', '.popup-design .popup-close');
+}
