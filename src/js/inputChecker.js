61d18982dbee7b67612d4d69cb9fc12d4dc8784c
hazartilirot <hazartilirot@gmail.com>
2021-08-03 10:21:12 +0200

    fixed regular expressions, input field shakes once an omitted characters are used

---
+++ src/js/inputChecker.js
@@ -0,0 +1,29 @@
+export default (selector, regexArray, errorMsg, classAtrError) => {
+  
+  const selectorAll = document.querySelectorAll(selector);
+  
+  selectorAll.forEach(i => {
+    i.addEventListener('input', () => {
+      let msg;
+      if (!regexArray.some(regex => regex.test(i.value))) {
+        i.value = i.value.substring(0, i.value.length - 1);
+        
+        i.classList.add('npt-error')
+        const form = i.parentNode;
+        msg = document.createElement('div');
+        msg.classList.add(`${classAtrError}`);
+        msg.style.marginTop = '20px';
+        form.appendChild(msg);
+
+        if (msg.previousSibling.className === `${classAtrError}`) {
+          msg.previousSibling.remove();
+        }
+
+        msg.textContent = errorMsg;
+
+        setTimeout(() => i.classList.remove('npt-error'), 500);
+        setTimeout(() => msg.remove(), 2500);
+      }
+    });
+  });
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,9 @@
 import modals from './modals/modals';
+import forms from './modals/forms';
 
 window.addEventListener('DOMContentLoaded', () => {
   "use strict";
   
   modals();
+  forms();
 })
